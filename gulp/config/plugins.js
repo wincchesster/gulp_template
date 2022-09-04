502d206e92aac5e856c6c53689441b37d5eadf75
Alexandr Chaikovsky <office2creative@gmail.com>
2022-09-04 22:23:37 +0300

    webp function added

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,12 @@
 import replace from "gulp-replace";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import browsersync from "browser-sync";
 
 
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browsersync
 }
--- gulpfile.js
+++ gulpfile.js
@@ -19,20 +19,23 @@ global.app = {
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import { scss } from './gulp/tasks/scss.js';
 
 
 // Wather function:
 function watcher() {
-    gulp.watch(path.watch.files, copy)
-    gulp.watch(path.watch.html, html)
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
 }
 
 
-const mainTasks = gulp.parallel(copy, html); 
+const mainTasks = gulp.parallel(copy, html, scss); 
 
 
 // Build the Scenario: 
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 // Execute the Scenario:
 gulp.task('default', dev);
